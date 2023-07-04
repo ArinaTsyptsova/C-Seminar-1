26beb14324187fd113125d21ff927219b0d79069
Arina <arinacypcova@gmail.com>
2023-07-04 19:29:50 +0700

    Задача 3

---
+++ Task 2/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Введите число от1 до 7");
+int number1 = int.Parse(Console.ReadLine());
---
+++ Task 3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+number = Math.Abs(number);
+
+Console.WriteLine("Целые числа в промежутке от -{0} до {0}:", number);
+
+for (int i=-number; i<number; i++){
+    Console.Write(i + ",");
+}
