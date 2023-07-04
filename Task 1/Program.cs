ae23c5fd7530c3e3174307cad820746cdd85923f
Arina <arinacypcova@gmail.com>
2023-07-04 18:37:19 +0700

    Задача 2 квадрат числа

---
+++ Task 1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число a: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int number2 = int.Parse(Console.ReadLine());
+int sqr = number2*number2;
+if (sqr == number1) {
+    Console.WriteLine($"{number1} является квадратом числа {number2}");
+}
+else {
+    Console.WriteLine($"{number1} не является квадратом числа {number2}");
+}
